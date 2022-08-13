c7bc7ea952d423fbd6309ab47bf85fd9f8308624
Jack <Jack@qq.com>
2022-08-13 10:31:02 +0800

    first commit

---
+++ src/main/java/com/zd/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zd.git;
+
+/**
+ * @author zhuda
+ * @create 2022-08-13 10:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
